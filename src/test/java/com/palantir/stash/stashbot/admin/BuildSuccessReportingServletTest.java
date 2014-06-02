89c3f1f22ed7fe5c9cf18758fb802984ecdcd48a
Matt Dee <mdee@palantir.com>
2014-06-02 11:00:19 -0700

    updating the metadata first to avoid inconsistent state for comment event handlers

--- src/test/java/com/palantir/stash/stashbot/admin/BuildSuccessReportingServletTest.java
+++ src/test/java/com/palantir/stash/stashbot/admin/BuildSuccessReportingServletTest.java
@@ -248,7 +248,7 @@ public class BuildSuccessReportingServletTest {
         Mockito.verify(prs).addComment(Mockito.eq(REPO_ID),
             Mockito.eq(PULL_REQUEST_ID), stringCaptor.capture());
         Mockito.verify(res).setStatus(200);
-        Mockito.verify(cpm).setPullRequestMetadata(pr.getId(), HEAD, MERGE_HEAD, null, true, null);
+        Mockito.verify(cpm).setPullRequestMetadata(PULL_REQUEST_ID, MERGE_HEAD, HEAD, null, true, null);
 
         String output = mockWriter.toString();
         Assert.assertTrue(output.contains("Status Updated"));
