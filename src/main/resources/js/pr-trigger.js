88f8c734b668c6bc90c630b0c18e37ae7a97b039
Carl Myers <cmyers@pernixdata.com>
2015-11-23 13:49:35 -0800

    Fix #36 - trigger PR build link broken after bitbucket migration
    Resources: https://confluence.atlassian.com/bitbucketserver/how-to-update-your-add-on-779302412.html
    https://developer.atlassian.com/bitbucket/concepts/javascript-api.html

--- src/main/resources/js/pr-trigger.js
+++ src/main/resources/js/pr-trigger.js
@@ -1,7 +1,7 @@
 // Look at /stash/plugins/servlet/stashbot/repo-status/$PROJECT/$REPO to figure out whether to display the link or not
 jQuery(document).ready(function () {
         console.debug("Loading Stashbot Trigger Code\n\n");
-        require(['model/page-state', 'util/navbuilder', 'jquery'], function(state, nav, $) {
+        require(['bitbucket/util/state', 'bitbucket/util/navbuilder', 'jquery'], function(state, nav, $) {
                 // TODO: if this ever breaks, see if these paths have changed...
                 // xpath = //*[@id="aui-page-panel-content-body"]/div/section/div/div/div[1]/div[2]
                 // css path = #aui-page-panel-content-body > div > section > div > div > div.aui-group > div.aui-item.summary-panel
@@ -12,10 +12,10 @@ jQuery(document).ready(function () {
                 var base_url = nav.pluginServlets().build();
                 console.debug("Base URL: " + base_url);
                 var pr = state.getPullRequest();
-                var pr_id = pr.getId();
-                var repo_id = pr.getToRef().getRepository().getId();
-                var from = pr.getFromRef().getLatestChangeset();
-                var to = pr.getToRef().getLatestChangeset();
+                var pr_id = pr.id;
+                var repo_id = pr.toRef.repository.id;
+                var from = pr.fromRef.latestCommit;
+                var to = pr.toRef.latestCommit;
                 var trigger_url = base_url + '/stashbot/build-trigger/' + repo_id + '/verify_pr/' + from + '/' + to + '/' + pr_id;
                 var url_split = document.URL.split("/");
                 var project = url_split[url_split.indexOf("projects") + 1];
